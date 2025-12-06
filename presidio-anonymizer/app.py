e9366e20740452d6c3f22933b03e8cc309d032ba
Yuechun Qin <Yqin2-cosc381@users.noreply.github.com>
2025-12-06 14:28:28 +0000

    remove whitespace from blank line to pass lint check

--- presidio-anonymizer/app.py
+++ presidio-anonymizer/app.py
@@ -44,7 +44,7 @@ class Server:
         def health() -> str:
             """Return basic health probe result."""
             return "Presidio Anonymizer service is up"
-        
+
         @self.app.route("/genz-preview")
         def genz_preview() -> str:
             """Return genz preview result."""
